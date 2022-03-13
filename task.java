ffb0bd8fa9ea5511f37a5d3aff1ec1b7606a9bf9
ralitsabasheva <100946717+ralitsabasheva@users.noreply.github.com>
2022-03-13 21:30:50 +0200

    Homework4

--- task.java
+++ task.java
@@ -1,77 +1,78 @@
-package task;
+package Homework;
 import java.util.Scanner;
-import java.lang.Math;
 
 public class task {
-    public static void main(String[] arg) {
-
-        /* Напишете програма, която отпечатва боят на буквите на
-        първото ви име, след това го отпечатайте веднъж само с главни
-        букви, и веднъж само с малки.
+    public static void main(String[] arg){
+        /*
+        Напишете програма :
+        Която извежда името ви в конзолата
          */
-        Scanner name = new Scanner(System.in);
-        System.out.println("Please enter your name:");
-        String myName = name.next();
-        System.out.println("Your name contains\t"  + myName.length() + "\tletters" );
-
-         /* Напишете програма, която да събира две числа и да отпечатва
-         резултата в конзолата.
-         */
-        Scanner firstNumber = new Scanner(System.in);
-        System.out.println("Please enter your first number");
-        long a = firstNumber.nextLong();
-
-        Scanner secondNumber = new Scanner(System.in);
-        System.out.println("Please enter your second number");
-        long b = secondNumber.nextLong();
-        System.out.println("The sum of the two numbers is\n" + (a+b));
-
+        Scanner myName = new Scanner(System.in);
+        System.out.println("Please enter your name:\t");
+        String name = myName.next();
+        System.out.println("Your name is :\t" + name);
 
         /*
-        Напишете програма, която да показва абсолютната стойност на
-        числата: -147, 15, 0, 18, резултата да се отпечатват на един и
-        съши ред, с разделение табулация.
+        Сравнява името с име на приятел/ка като използвате equals(), equalsIgnoreCase(), compareTo(),
+        езултатите да излизат на отделни редове
          */
 
-        int firstNum = Math.abs(-147);
-        int secondNum= Math.abs(15);
-        int thirdNum = 0;
-        int fourthNum = Math.abs(15);
-        int fifthNum = Math.abs(18);
+        Scanner friendName = new Scanner(System.in);
+        System.out.println("Please enter your friend's name:");
+        String frName = friendName.next();
+        System.out.println("Your friend's name is :\t" + frName);
+        boolean equalsNane = true;
 
-        System.out.print("The absolute values are :\t" + fifthNum + "\t" + secondNum + "\t" + thirdNum + "\t" + fourthNum + "\t" + firstNum);
+        if(frName.equals(name) == equalsNane) {
+                System.out.println("The names match");
+    }
+        else
+        {
+            System.out.println("The names do not match");
+        }
 
-        /*
-        Напишете програма, която изчислява корен квадратен на
-        числата: 8, 353, 78, 3, -67, 723, 2000, -100, резултатите да се
-        отпечатват на един и същи ред.
-         */
-
-        double squareRoot1 = Math.sqrt(8);
-        double squareRoot2 = Math.sqrt(353);
-        double squareRoot3 = Math.sqrt(78);
-        double squareRoot4 = Math.sqrt(3);
-        double squareRoot5 = Math.sqrt(-67);
-        double squareRoot6 = Math.sqrt(723);
-        double squareRoot7 = Math.sqrt(2000);
-        double squareRoot8 = Math.sqrt(-100);
+        if(frName.equalsIgnoreCase(name) == equalsNane) {
+            System.out.println("The names match");
+        }
+        else
+        {
+            System.out.println("The names do not match");
+        }
 
 
-        System.out.print("\n" + "Тhe square root of the numbers are:\n" + squareRoot1+ "\t" + squareRoot2 + "\t" + "\t" +squareRoot3 +"\t" + squareRoot4 + "\t" + squareRoot5 + "\t" + squareRoot6 + "\t" + squareRoot7 + "\t" + squareRoot8 );
+        int compareName = name.compareTo(frName);
+        if (compareName == 0){
+            System.out.println("The names match");
+        }
+        else if(compareName > 0){
+            System.out.println("My name's  bigger than my friend");
+        }
+        else{
+            System.out.println("My name's  less than my friend");
+        }
 
-        /*
-        Напишете програма, която повдига на степен 2-ра числата: -20,
-        92, 638, 73, 200, -22, резултатите да се отпечатват на един и
-        същи ред.
-         */
-        double number1 = Math.pow(-20,2);
-        double number2= Math.pow(92,3);
-        double number3 = Math.pow(638,2);
-        double number4 = Math.pow(73,2);
-        double number5 = Math.pow(200,2);
-        double number6 = Math.pow(-22,2);
+        Scanner lastName = new Scanner(System.in);
+        System.out.println("Please enter your last name");
+        String myLastName = lastName.next();
+        System.out.println("My surname is :\t" + myLastName);
+        System.out.println("Мy first and last name are:\t" + name + "\t"+ myLastName );
 
-        System.out.print("\n" + "The numbers of the second degree are" +"\n" +number1 + "\t" + number2 + "\t" + number3 + "\t" + number4 + "\t" + number5 + "\t" + number6);
+        Scanner age = new Scanner(System.in);
+        System.out.println("Please enter your age: \t");
+        int myAge = age.nextInt();
+        System.out.println("I'm\t" + myAge + "years old");
+        System.out.println("My name's :\t" + name + "\t" + myLastName + "and I'm\t" + myAge + "years old" );
+
+        Scanner frLastName = new Scanner(System.in);
+        System.out.println("[lease enter your friend's last name:\t ");
+        String friendLastName = frLastName.next();
+        Scanner frAge = new Scanner(System.in);
+        System.out.println("Please enter your friend's age");
+        int myFriendAge = frAge.nextInt();
+        System.out.println("My friend's name is: \t" + frName + "\t" + friendLastName + "\t" + "He/She is\t" + myFriendAge + "\t" +"\t" + "years old");
+
+
+
+
     }
 }
-
